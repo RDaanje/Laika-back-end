80c464c2cbbc9b2042149a0b300eaa4881db0b87
RDaanje <rdaanje@gmail.com>
2019-07-30 11:24:21 +0200

    first commit

---
+++ src/main/java/nl/YoungCapital/Laika/LaikaApplication.java
@@ -0,0 +1,13 @@
+package nl.YoungCapital.Laika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaikaApplication.class, args);
+	}
+
+}
